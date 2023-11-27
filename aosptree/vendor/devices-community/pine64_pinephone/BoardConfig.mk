22a713ae8fee3bbf246642411fa0fe97e51b384e
Roman Stratiienko <r.stratiienko@gmail.com>
2023-11-27 09:04:49 +0200

    Upgrade the kernel to v6.1
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- aosptree/vendor/devices-community/pine64_pinephone/BoardConfig.mk
+++ aosptree/vendor/devices-community/pine64_pinephone/BoardConfig.mk
@@ -8,27 +8,27 @@ include glodroid/configuration/common/board-common.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(BC_PATH)/sepolicy/vendor
 
-BOARD_KERNEL_SRC_DIR := glodroid/kernel/common-android13-5.15-lts
+BOARD_KERNEL_SRC_DIR := glodroid/kernel/common-android14-6.1-lts
 KERNEL_DEFCONFIG := $(BC_PATH)/kernel.defconfig
 KERNEL_FRAGMENTS := glodroid/configuration/platform/common/sunxi/a64_overlay.config
 KERNEL_DTB_FILES := allwinner/sun50i-a64-pinephone-1.1.dtb allwinner/sun50i-a64-pinephone-1.2.dtb
 
 BOARD_KERNEL_PATCHES_DIRS := \
-    $(BC_PATH)/patches-kernel/megi-audio-5.15     \
-    $(BC_PATH)/patches-kernel/megi-bt-5.15        \
-    $(BC_PATH)/patches-kernel/megi-drm-5.15       \
-    $(BC_PATH)/patches-kernel/megi-fixes-5.15     \
-    $(BC_PATH)/patches-kernel/megi-pp-5.15        \
-    $(BC_PATH)/patches-kernel/megi-samuel-5.15    \
-    $(BC_PATH)/patches-kernel/megi-wifi-5.15      \
-    $(BC_PATH)/patches-kernel/megi-anx-5.15       \
-    $(BC_PATH)/patches-kernel/megi-axp-5.15       \
-    $(BC_PATH)/patches-kernel/megi-cam-5.15       \
-    $(BC_PATH)/patches-kernel/megi-err-5.15       \
-    $(BC_PATH)/patches-kernel/megi-modem-5.15     \
-    $(BC_PATH)/patches-kernel/megi-speed-5.15     \
-    $(BC_PATH)/patches-kernel/glodroid-5.15       \
-    glodroid/configuration/patches/kernel/android13-5.15-2023-01/sun4i-drm \
+    $(BC_PATH)/patches-kernel/megi-audio-6.1     \
+    $(BC_PATH)/patches-kernel/megi-bt-6.1        \
+    $(BC_PATH)/patches-kernel/megi-drm-6.1       \
+    $(BC_PATH)/patches-kernel/megi-fixes-6.1     \
+    $(BC_PATH)/patches-kernel/megi-pp-6.1        \
+    $(BC_PATH)/patches-kernel/megi-samuel-6.1    \
+    $(BC_PATH)/patches-kernel/megi-wifi-6.1      \
+    $(BC_PATH)/patches-kernel/megi-anx-6.1       \
+    $(BC_PATH)/patches-kernel/megi-axp-6.1       \
+    $(BC_PATH)/patches-kernel/megi-cam-6.1       \
+    $(BC_PATH)/patches-kernel/megi-err-6.1       \
+    $(BC_PATH)/patches-kernel/megi-modem-6.1     \
+    $(BC_PATH)/patches-kernel/megi-speed-6.1     \
+    $(BC_PATH)/patches-kernel/glodroid-6.1       \
+    $(BC_PATH)/patches-kernel/glodroid-drm-6.1   \
 
 GD_BOOTSCRIPT_OVERLAY_DEVICE := $(BC_PATH)/boot/bootscript_device_overlay.h
 
