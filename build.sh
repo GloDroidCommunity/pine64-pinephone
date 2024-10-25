0d901f55d261985a5b5cbb02204139e7e596dc02
Roman Stratiienko <r.stratiienko@gmail.com>
2024-10-25 13:39:41 +0300

    Android-15 bring-up
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ trap 'echo -e "\nbuild.sh interrupted"; exit 1' SIGINT
 echo Building the Android
 pushd aosptree
 . build/envsetup.sh
-lunch pine64_pinephone-userdebug
+lunch pine64_pinephone-ap3a-userdebug
 make images -k || make images -j1
 make sdcard
 popd
